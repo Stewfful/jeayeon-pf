396782db3a53ef0f18a7597802e30f1635724336
GG227009 <moira9508@gmail.com>
2024-06-25 11:14:35 +0900

    수정6

--- app/page.tsx
+++ app/page.tsx
@@ -155,12 +155,7 @@ export default function Home() {
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
       <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Jeayeon-Park. All rights reserved.</p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-        <Link className="text-xs hover:underline underline-offset-4" href="#">
-          Privacy
-        </Link>
-        <Link className="text-xs hover:underline underline-offset-4" href="#">
-          Terms of Service
-        </Link>
+        
       </nav>
     </footer>
   </div>
